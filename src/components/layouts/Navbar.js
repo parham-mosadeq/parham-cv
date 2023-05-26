1e37bc9e67e1b2ce2ceba41dc4341ce444707f97
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-05-26 23:36:07 +0430

    debug nav

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,11 +1,16 @@
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { HiMenu, HiX } from 'react-icons/hi';
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handleMenu = () => setIsOpen((prev) => !prev);
   const navItems = ['about me', 'resume', 'blog', 'contact'];
+
+  useEffect(() => {
+    handleMenu();
+  }, [window.location]);
+
   return (
     <header className='w-screen h-16 flex bg-mainDark relative'>
       <nav className='w-full flex items-center justify-between mx-auto max-w-7xl px-5 md:px-0'>
@@ -23,16 +28,17 @@ const Navbar = () => {
           </Link>
         </div>
         {/* btn */}
-        <div
-          onClick={() => handleMenu()}
-          className='text-mainWhite text-2xl cursor-pointer z-10 md:hidden'
-        >
-          {isOpen ? <HiMenu /> : <HiX />}
+        <div className='text-mainWhite text-2xl cursor-pointer z-10 md:hidden'>
+          {isOpen ? (
+            <HiMenu onClick={() => handleMenu()} />
+          ) : (
+            <HiX onClick={() => handleMenu()} />
+          )}
         </div>
         {/* btn */}
         <ul
           className={`${
-            isOpen ? 'hidden' : 'absolutes'
+            isOpen ? 'hidden' : 'absolute'
           } text-mainWhite bg-mainDark h-screen w-1/2 capitalize absolute top-0 right-0 flex flex-col items-center justify-center
           
           md:flex md:flex-row md:h-16 md:w-1/2 md:mx-auto
