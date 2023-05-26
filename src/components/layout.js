bd8d61d24f349712352586dd5a504071f5f394e3
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-05-26 23:27:06 +0430

    design layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Layout = ({ children }) => {
-  return <main className=''>{children}</main>;
+  return (
+    <main className='w-full px-5 m-0 mx-auto md:max-w-5xl md:p-0 '>
+      {children}
+    </main>
+  );
 };
 
 export default Layout;
