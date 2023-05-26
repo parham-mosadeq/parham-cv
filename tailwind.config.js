a8e06f7fe497e16aedabc4df1ab1446cfe12cff0
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-05-26 22:28:54 +0430

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        mainDark: '#222222',
+        mainGray: '#959395',
+        mainBlue: '#007CED',
+        mainWhite: '#F5F5F5',
+        mainShadow: '#F1F1F1',
+        secBlue: '#0476DE',
+        secGray: '#4F4F4F',
+      },
+    },
+  },
+  plugins: [],
+};
