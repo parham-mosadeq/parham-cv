3b0957cfb1c6cb76636031569b17ec8454cc0dc3
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-05-29 00:21:01 +0430

    optimize classes and add socials

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -3,16 +3,29 @@ import Layout from './layout';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear().toString();
+  const sharedClasses = `flex items-center justify-center`;
+  const linkClasses=`hover:text-mainGray transition duration-500`
   return (
     <Layout>
-      <footer className='flex items-center justify-center text-mainWhite '>
-        <h2>
+      <footer
+        className={`${sharedClasses}  flex-col  text-mainWhite border-t  border-mainGray`}
+      >
+        <h2 className='my-4'>
           All right's reserved &copy;
           <span className=''>2021 - {currentYear} | </span>
           <span className='text-mainBlue tracking-wider font-bold'>
             Parham Mosadeqzadeh
           </span>
         </h2>
+        {/* socials */}
+        <div className={`${sharedClasses} flex-row gap-3 my-4`}>
+          <a className={linkClasses} href='/' target='_blank'>
+            linkedin
+          </a>
+          <a className={linkClasses} href='/' target='_blank'>
+            github
+          </a>
+        </div>
       </footer>
     </Layout>
   );
