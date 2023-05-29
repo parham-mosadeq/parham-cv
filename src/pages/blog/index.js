5bbcfae516050cc0e6ea02f0a133fba1662f257f
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-05-29 23:39:57 +0430

    create blog page

---
+++ src/pages/blog/index.js
@@ -0,0 +1,14 @@
+import Layout from '@/components/layouts/layout';
+import React from 'react';
+
+const BlogPage = () => {
+  return (
+    <Layout>
+      <div className=' h-screen text-center'>
+        <h3 className='text-5xl py-10 text-secBlue uppercase'>coming soon</h3>
+      </div>
+    </Layout>
+  );
+};
+
+export default BlogPage;
